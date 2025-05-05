a22675e6e29154af2888e55772224bebe6a838ed
Reza Sharifi <reza.sharifi76@gmail.com>
2025-05-05 18:22:59 +0330

    add LoadingView

--- Menuva/Core/LoadingView.swift
+++ Menuva/Core/LoadingView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var start = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Text("Loading...")
+                .font(.largeTitle.bold())
+                .foregroundStyle(.black)
+            
+            Text("Loading...")
+                .font(.largeTitle.bold())
+                .foregroundStyle(.white)
+                .frame(width: 200, height: 50)
+                .background(Color.black)
+                .mask {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                        .offset(x: start ? -60 : 60)
+                }
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 1)
+                .repeatForever(autoreverses: true)) {
+                    start = true
+                }
+        }
     }
 }
 
